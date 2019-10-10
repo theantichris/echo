2fd468a14cad7d65384336628752214c302caf12
Christopher Lamm <chris@theantichris.com>
2019-10-10 11:51:33 -0400

    using strings.Join()

--- echo.go
+++ echo.go
@@ -3,16 +3,12 @@ package echo
 import (
 	"fmt"
 	"io"
+	"strings"
 )
 
 // Echo writes out a list of strings separated by spaces.
 func Echo(args []string, w io.Writer) {
-	var output, separator string
-
-	for _, arg := range args[1:] {
-		output += separator + arg
-		separator = " "
-	}
+	output := strings.Join(args[1:], " ")
 
 	fmt.Fprintln(w, output)
 }
